3f1726111219c39f70f37f9dc7f1c97b711ea272
s1528459 <s.v.e.van.der.putten@umail.leidenuniv.nl>
2020-10-11 19:38:28 +0200

    Updated script and created local setup

--- das5setup.sh
+++ das5setup.sh
@@ -2,12 +2,23 @@
 
 root_dir=$(pwd)
 
+if [ -z "$SPARK_HOME" ]
+then 
+  SPARK_HOME=~/lib/spark
+fi
+
+if [ -z "$HADOOP_HOME" ]
+then 
+  HADOOP_HOME=~/lib/hadoop
+fi
+
 initial_setup() {
     echo "Starting setup"
     # echo "$root_dir"
     mkdir -p ~/lib
 }
 
+# ssh keys to all nodes
 setup_nodes(){
   for node in "${nodes[@]}"
   do
@@ -17,6 +28,7 @@ setup_nodes(){
 
 }
 
+# used on the das5
 if [[ $1 = "--nodes" ]]
 then
   echo "Setting up a node cluster of size $2"
@@ -29,16 +41,51 @@ then
   echo "Configuring nodes. Please enter your password."
   ssh-keygen -t rsa -P "" -f ~/.ssh/id_rsa
   setup_nodes
+  for node in "${nodes[@]}"
+  do
+    ssh node 'cd $SPARK_HOME && sbin/start-slave.sh spark://fs1.cm.cluster:7082'
+    echo "$node"
+  done
   
   exit 0
 fi
 
+# Mostly for local debugging
 if [[ $1 = "--local" ]]
 then
   echo "Setting up a node cluster of size 1"
+  echo "Setting up Hadoop & Spark"
+  #initial_setup
+  $SPARK_HOME/sbin/start-all.sh 
+  $HADOOP_HOME/sbin/start-all.sh 
 
   exit 0
 fi
+
+stop_all () {
+    $SPARK_HOME/sbin/stop-all.sh
+    $HADOOP_HOME/sbin/stop-all.sh
+}
+
+if [[ $1 = "--stop-all" ]]
+then
+  echo "Stopping all"
+  stop_all
+  exit 0
+fi
+
+start_all () {
+    $SPARK_HOME/sbin/start_all.sh
+    $HADOOP_HOME/sbin/start_all.sh
+}
+
+if [[ $1 = "--start-all" ]]
+then
+  echo "Stopping all"
+  start_all
+  exit 0
+fi
+
 # start script
 # initial_setup
 #start_master () {
@@ -53,11 +100,4 @@ fi
 #    ~/.local/bin/spark/sbin/start-slaves.sh
 #}
 #
-#start () {
-#    ~/.local/bin/spark/sbin/start-master.sh
-#    ~/.local/bin/spark/sbin/start-slaves.sh
-#}
-#
-#stop_all () {
-#    ~/.local/bin/spark/sbin/stop-all.sh
-#}
+
